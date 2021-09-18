a4042e959a10025cab6ed5ed7bbaa33e48e3353f
Anjaney C Mahajan <acmahaja@gmail.com>
2021-09-18 09:13:51 +0200

    Moved methods to files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+const errorMessage = import('./modules/error.js')
+const { pingMessage, pongMessage} = import('./modules/game.js')
 
 const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -11,104 +13,6 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-const errorMessage = (message, error) => {
-    console.log(error);
-    const exampleEmbed = new MessageEmbed()
-        .setColor('#ef3939')
-        .setTitle('Something Broke 😲')
-        .setFooter(`PremBOT`, 'https://raw.githubusercontent.com/acmahaja/PremBOT/master/logo.png')
-        .setDescription(`${error}`)
-        .setTimestamp()
-    message.channel.send({ embeds: [exampleEmbed] });
-}
-
-const pingMessage = (message) => {
-    const exampleEmbed = new MessageEmbed()
-        .setColor('#e6b48f')
-        .setTitle('Ping 🏓')
-        .setFooter(`PremBOT`, 'https://raw.githubusercontent.com/acmahaja/PremBOT/master/logo.png')
-        .setTimestamp()
-    message.channel.send({ embeds: [exampleEmbed] });
-}
-
-const pongMessage = (message) => {
-    const exampleEmbed = new MessageEmbed()
-        .setColor('#e6b48f')
-        .setTitle('Pong 🏓')
-        .setFooter(`PremBOT`, 'https://raw.githubusercontent.com/acmahaja/PremBOT/master/logo.png')
-        .setTimestamp()
-    message.channel.send({ embeds: [exampleEmbed] });
-}
-
-
-const playerEmbed = (data) => {
-    return new MessageEmbed()
-        .setColor('#562765')
-        .setFooter(`${data.player_first_name} ${data.player_last_name}`, 'https://raw.githubusercontent.com/acmahaja/PremBOT/master/logo.png')
-        .setTitle(`${data.name}`)
-        .addFields(
-            {
-                name: `${data.summary_overall_points}`,
-                value: 'Total Points',
-            },
-            {
-                name: `${data.summary_overall_rank}`,
-                value: 'Overall Rank',
-            },
-            {
-                name: `${data.summary_event_rank}`,
-                value: `Week ${data.current_event}`,
-            },
-            { name: 'Classic Leagues', value: `${data.leagues.classic.length}`, inline: true },
-            { name: 'h2h', value: `${data.leagues.h2h.length}`, inline: true },
-
-        );
-}
-
-const overviewEmbed = (data) => {
-
-    let fields = []
-    let i = 0;
-    while (data.events[i].finished && i <38) {
-
-        fields = fields.concat(
-            [{
-                name: `${data.events[i].name}`,
-                value: `Week`,
-                inline: true
-            },
-            {
-                name: `${data.events[i].average_entry_score}`,
-                value: `Average`,
-                inline: true,
-            },
-            {
-                name: `${data.events[i].average_entry_score}`,
-                value: `Highest Score`,
-                inline: true,
-            },
-            {
-                name: `${data.events[i].transfers_made}`,
-                value: `Transfers Made`,
-                inline: true,
-            }
-        ])
-        i++;
-    }
-
-    return new MessageEmbed()
-        .setColor('#562765')
-        .setFooter(`PremBOT`, 'https://raw.githubusercontent.com/acmahaja/PremBOT/master/logo.png')
-        .setTitle(`Overview`)
-        .addFields(
-            {
-                name: `${data.total_players}`,
-                value: 'Total Players',
-            }
-            ,fields
-        );
-}
 
 client.on('messageCreate', async(message) => {
     if (message.content[0] === '~') {
@@ -154,25 +58,7 @@ client.on('messageCreate', async(message) => {
     }
 });
 
+
+
 // Login to Discord with your client's token
 client.login(process.env.TOKEN);
-
-
-// http.get('https://fantasy.premierleague.com/api/bootstrap-static/')
-//     .then(
-//         function(response) {
-//             if (response.status !== 200) {
-//                 console.log('Looks like there was a problem. Status Code: ' +
-//                     response.status);
-//                 return;
-//             }
-
-//             // Examine the text in the response
-//             response.json().then(function(data) {
-//                 console.log(data);
-//             });
-//         }
-//     )
-//     .catch(function(err) {
-//         console.log('Fetch Error :-S', err);
-//     });
---
+++ modules/game.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
+const pingMessage = (message) => {
+    const exampleEmbed = new MessageEmbed()
+        .setColor('#e6b48f')
+        .setTitle('Ping 🏓')
+        .setFooter(`PremBOT`, 'https://raw.githubusercontent.com/acmahaja/PremBOT/master/logo.png')
+        .setTimestamp()
+    message.channel.send({ embeds: [exampleEmbed] });
+}
+
+const pongMessage = (message) => {
+    const exampleEmbed = new MessageEmbed()
+        .setColor('#e6b48f')
+        .setTitle('Pong 🏓')
+        .setFooter(`PremBOT`, 'https://raw.githubusercontent.com/acmahaja/PremBOT/master/logo.png')
+        .setTimestamp()
+    message.channel.send({ embeds: [exampleEmbed] });
+}
+
+
+module.exports = {pingMessage, pongMessage}
